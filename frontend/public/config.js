ae92a286596d1611f4fa9fe1d95ac06595f965e2
root <root@praveen.asia-south2-c.c.psychic-expanse-454404-g2.internal>
2025-05-04 17:43:20 +0000

    added k8s config

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,4 +1,4 @@
 window.RUNTIME_CONFIG = {
-  VITE_AUTH_API_URL: "http://localhost:8081/auth",
-  VITE_TASKS_API_URL: "http://localhost:8083/tasks"
+  VITE_AUTH_API_URL: "http://34.131.79.242:8081/auth",
+  VITE_TASKS_API_URL: "http://34.131.79.242:8083/tasks"
 }; 
